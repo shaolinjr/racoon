5c807a2f9b9a08d18504439a7caa64ebcc85496a
Arthur Pires <arthurclp@icloud.com>
2020-06-12 12:09:31 -0300

    Deleted old error.js files inside dist because they were causing reference errors

--- dist/errors.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WrongSchemaFormatError = () => new Error("Check your schema, it seems to have an error with the format/type you used.");
